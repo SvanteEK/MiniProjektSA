a8dbbba6376c41cc4b1e3c60c0a48b7fcf9e628a
MikeB300 <eaa24mbosm@students.eaaa.dk>
2026-02-26 10:27:02 +0100

    Update DataService.cs

--- MiniProjektSA/Services/DataService.cs
+++ MiniProjektSA/Services/DataService.cs
@@ -86,8 +86,7 @@ public class DataService
      
      public void UpvoteComment(int postid, int commentid)
      {
-         var post = db.Posts.Where(p => p.Id == commentid && p.MotherPostId == postid).FirstOrDefault();
-         var comment = db.Posts.Where(p => p.Id == commentid).FirstOrDefault();
+         var comment = db.Posts.Where(p => p.Id == commentid && p.MotherPostId == postid).FirstOrDefault();
          if (comment.Votescore == null)
          {
              comment.Votescore = 1;
@@ -100,8 +99,7 @@ public class DataService
      
      public void DownvoteComment(int postid, int commentid)
      {
-         var post = db.Posts.Where(p => p.Id == commentid && p.MotherPostId == postid).FirstOrDefault();
-         var comment = db.Posts.Where(p => p.Id == commentid).FirstOrDefault();
+         var comment = db.Posts.Where(p => p.Id == commentid && p.MotherPostId == postid).FirstOrDefault();
          if (comment.Votescore == null)
          {
              comment.Votescore = -1;
